c92542284bedd4b932167f554be82d9e8ce0ba4b
gimenezes <menezes.s.gilda@gmail.com>
2021-04-09 15:43:23 -0300

    create project

---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+*{
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html{
+  font-size: 62.5%;
+}
+
+html, body, #__next{
+  height: 100%;
+}
+
+body{
+  font-family: 'Quicksand', sans-serif;
+}
+
+button{
+  cursor: pointer;
+}
+
+`;
+
+export default GlobalStyles;
